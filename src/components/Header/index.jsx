34ed99d5c2980e3682705134eb9857fa8bc65505
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-04-04 10:04:39 -0500

    Fixing page header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,11 @@ import styles from './Header.module.css'
 export default function Header () {
   return (
     <div className={styles.Header}>
-      <img src={logoRandress} alt='logo-randress' />
+      {/* <img src={logoRandress} alt='logo-randress' /> */}
+      <div className={styles.TitleContainer}>
+        <div className={styles.Title}>Memory App</div>
+        <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif' alt='logo-randress' className={styles.Charizard} />
+      </div>
       <a href='https://github.com/RANDRESS23/memory-app-pokemon' target='__blank'>
         <FaGithub className={styles.IconGitHub} />
       </a>
